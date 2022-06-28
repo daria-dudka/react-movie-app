178cb1dab8388c77c9b9086767515cc1caf517ea
daria-dudka <d.svirskaya@gmail.com>
2022-06-28 16:39:57 +0100

    fixed the width of img

--- src/pages/SingleMovie.js
+++ src/pages/SingleMovie.js
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
+import classes from './SingleMovie.module.css';
 
 function SingleMoviePage(props) {
   return (
     <section>
       <div>
-        <img src={props.movie.moviebanner} alt={props.movie.title} />
+        <img
+          className={classes.image}
+          src={props.movie.moviebanner}
+          alt={props.movie.title}
+        />
         <h3 className='title'>{props.movie.title}</h3>
         <p>Director:{props.movie.director}</p>
         <p>Release date:{props.movie.releasedate}</p>
       </div>
-
       <Link className='btn single__btn' to='/react-movie-app/'>
         Back
       </Link>
